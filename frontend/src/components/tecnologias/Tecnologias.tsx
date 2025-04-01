ce3115c2003f1b66e2a0d71b3221ac604eef4023
Alessandro Carvalho de Souza <alesouzadf@gmail.com>
2025-03-31 21:02:09 -0300

    feat(frontend): inclui chave key em tecnlogias

--- frontend/src/components/tecnologias/Tecnologias.tsx
+++ frontend/src/components/tecnologias/Tecnologias.tsx
@@ -10,7 +10,7 @@ export default function Tecnologias(props: TecnologiasProps) {
 		<div className="flex justify-center gap-4 flex-wrap w-4/5">
 			{props.lista.map((tecnologia) => {
 				return (
-					<div className="flex flex-col items-center gap-1">
+					<div key={tecnologia.id} className="flex flex-col items-center gap-1">
 						<span className="relative h-11 w-11 sm:h-16 sm:w-16 rounded-xl overflow-hidden">
 							<Image
 								src={tecnologia.imagem}
